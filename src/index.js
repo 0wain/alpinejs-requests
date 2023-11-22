0800d616ed63d983f3619454f66f3105c8af6d07
Owain Macbook <owain.jones74@gmail.com>
2023-11-22 19:08:57 +0000

    refactor to support multiple request types

--- src/index.js
+++ src/index.js
@@ -1,45 +1,73 @@
 const Plugin = function (Alpine) {
-    Alpine.directive('post', (el, { expression }, { evaluate, cleanup }) => {
-        // Support for just a route or an object of data
+    const methods = [
+        {
+            directive: 'request'
+        },
+        {
+            directive: 'post',
+            method: 'POST'
+        },
+        {
+            directive: 'get',
+            method: 'GET'
+        },
+    ]
+
+    methods.forEach((type) => {
+        Alpine.directive(type.directive, (el, { expression }, { evaluate, cleanup }) => {
+            directiveSetup({el, method: type.method, expression, evaluate})
+            cleanup(() => observer.disconnect())
+        })
+        Alpine.magic(type.directive, (el, { evaluate }) => expression =>{
+            let data = evalExpression(expression, evaluate);
+            processRequest({el, ...data});
+        })
+    });
+
+
+
+    function directiveSetup({el, method, expression, evaluate}) {
         let data = evalExpression(expression, evaluate);
 
-
-        // Listen for the click event
         el.addEventListener('click', () => {
             processRequest({
                 el,
+                method,
                 ...data
             });
         })
+    }
 
-        cleanup(() => observer.disconnect())
-    })
-    Alpine.magic('post', (el, { evaluate }) => expression =>{
-        let data = evalExpression(expression, evaluate);
-        processRequest({
-            el,
-            ...data
-        });
-    })
-
-    function processRequest({el, route, headers, body}) {
+    function processRequest({el, method, route, headers, body}) {
+        console.log({el, method, route, headers, body});
         // Make the request
         fetch(route, {
             // Maybe allow this to be dynamic in the future?
-            method: "POST",
+            method: method ?? "POST",
             headers,
             body
         }).then((response) => {
-            // Trigger the @posted event.
-            el.dispatchEvent(new CustomEvent('posted', {
+            // Trigger the @request event.
+            el.dispatchEvent(new CustomEvent('request', {
+                state:'success',
+                response
+            }))
+            // Trigger the @ event.
+            el.dispatchEvent(new CustomEvent((method ?? 'post').toLowerCase(), {
                 state:'success',
                 response
             }))
         }).catch((error) => {
             console.warn(error);
-            el.dispatchEvent(new CustomEvent('posted', {
-                state: 'error',
-                error
+            // Trigger the @request event.
+            el.dispatchEvent(new CustomEvent('request', {
+                state:'error',
+                response
+            }))
+            // Trigger the @ event.
+            el.dispatchEvent(new CustomEvent((method ?? 'post').toLowerCase(), {
+                state:'error',
+                response
             }))
         })
     }
